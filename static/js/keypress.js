2caadcb38e9dc6c3d298d56809c558cf30016887
Jeremiah Wibberding <chipwibb@gmail.com>
2019-01-07 19:22:36 -0800

    ROOMS!

--- static/js/keypress.js
+++ static/js/keypress.js
@@ -9,16 +9,16 @@ window.addEventListener("keydown", function (event) { changeKeyState(event, true
 window.addEventListener("keyup", function (event) { changeKeyState(event, false); });
 
 window.addEventListener("mousedown", function (event) {
+  if (gameRunning) {
+    socket.emit('sound_effect', "shootSound");
+  }
+  
   var angle = 0;
   angle = Math.atan2(event.clientY - (canvas.height / 2), event.clientX - (canvas.width / 2));
 
   socket.emit('mousedown', {
     angle: angle
   });
-
-  if (gameRunning) {
-    socket.emit('sound_effect', "shootSound");
-  }
 });
 
 var Mouse = {
